1c545e93cefc4caedb27144b86109c92f1d77077
inex <38869502+1nexoravel@users.noreply.github.com>
2025-12-08 11:07:54 -0300

    Atualizacao automatica PT-BR via Crowdin

--- auto.bat
+++ auto.bat
@@ -3,12 +3,18 @@ call crowdin download sources
 call crowdin download translations -l pt-BR
 
 set "DESTINO=C:\Users\Ruy_R\OneDrive\Documents\GitHub\Usuimo-PTBR\PTBR"
+set "DESTINO2=E:\sister-windows-beta\www\data\PTBR"
 
 if not exist "%DESTINO%" mkdir "%DESTINO%"
 
 for %%F in (*PTBR*.json) do (
     move "%%F" "%DESTINO%"
 )
+
+for %%F in (*PTBR*.json) do (
+    copy "%%F" "%DESTINO2%"
+)
+
 
 del /f /q *EN*.json
 
@@ -18,7 +24,7 @@ git diff --cached --quiet
 if %errorlevel%==0 (
     echo Nenhuma alteracao detectada. Commit nao criado.
 ) else (
-    git commit -m "Atualização automatica PT-BR via Crowdin"
+    git commit -m "Atualizacao automatica PT-BR via Crowdin"
 )
 git fetch origin
 git push
